e64b9b078727e1c4d1d47450421870797844ec1e
Simone Baptiste <baptistesimone19@gmail.com>
2024-11-11 22:05:32 +0100

    utils functions

--- utils/utils.go
+++ utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"regexp"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
@@ -14,4 +15,30 @@ func IsValidEmail(email string) bool {
 func HashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
+}
+
+func SplitCamelCase(s string) string {
+	re := regexp.MustCompile(`([a-z0-9])([A-Z])`)
+	split := re.ReplaceAllString(s, `$1 $2`)
+	return ToLowercaseFirstLetter(split)
+}
+
+func ToSnakeCase(str string) string {
+	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
+}
+
+func ToLowercaseFirstLetter(s string) string {
+	words := strings.Split(s, " ")
+	for i, word := range words {
+		if i == 0 {
+			continue
+		}
+		words[i] = strings.ToLower(word[:1]) + word[1:]
+	}
+	return strings.Join(words, " ")
 }
