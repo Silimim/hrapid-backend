8f50438b232800e1a63f114fd865fad349f54c4a
Simone Baptiste <baptistesimone19@gmail.com>
2024-12-03 17:17:10 +0100

    debug utilities and context utilities

--- utils/utils.go
+++ utils/utils.go
@@ -1,11 +1,20 @@
 package utils
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 	"regexp"
 	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
+
+type ContextKey string
+
+const UserKey ContextKey = "user"
 
 func IsValidEmail(email string) bool {
 	re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
@@ -41,4 +50,45 @@ func ToLowercaseFirstLetter(s string) string {
 		words[i] = strings.ToLower(word[:1]) + word[1:]
 	}
 	return strings.Join(words, " ")
+}
+
+func DebugRequest(r *http.Request) {
+	log.Printf("Request Method: %s", r.Method)
+	log.Printf("Request URL: %s", r.URL.String())
+	log.Printf("Request Headers:")
+	for key, values := range r.Header {
+		log.Printf("  %s: %v", key, values)
+	}
+
+	log.Println("Context values:")
+	PrintContextInternals(r.Context(), 0)
+}
+
+func PrintContextInternals(ctx context.Context, depth int) {
+	indent := fmt.Sprintf("%*s", depth*2, "")
+
+	log.Printf("%sContext type: %T", indent, ctx)
+
+	if val := ctx.Value(UserKey); val != nil {
+		log.Printf("%sUser value found: %+v", indent, val)
+	} else {
+		log.Printf("%sNo user value found with key 'user'", indent)
+	}
+
+	if nextCtx := ctx.Value(struct{}{}); nextCtx != nil {
+		if nextCtx != ctx {
+			PrintContextInternals(nextCtx.(context.Context), depth+1)
+		}
+	}
+}
+
+func IsPointerType(fieldType string) bool {
+	return len(fieldType) > 0 && fieldType[0] == '*'
+}
+
+func SendJSONResponse(w http.ResponseWriter, data interface{}) {
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
